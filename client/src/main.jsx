031467cf2e58a7330eaa77c88f95be3ea466760c
Yash Singhal <singhaly914@gmail.com>
2024-10-16 10:33:43 +0530

    add

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,23 +4,11 @@ import App from "./App.jsx";
 import "./index.css";
 import { ScrollProvider } from "./context/ScrollContext.jsx";
 import { Auth0Provider } from "@auth0/auth0-react";
-// import { ClerkProvider } from "@clerk/clerk-react";
-
-// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-// import { useNavigate } from "react-router-dom";
-// const navigate = useNavigate();
-
-// const onRedirectCallback = (appState) => {
-//   navigate(appState?.returnTo || window.location.pathname);
-// };
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ScrollProvider>
-      {/* <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/"> */}
         <App />
-      {/* </ClerkProvider> */}
     </ScrollProvider>
   </StrictMode>
 );
