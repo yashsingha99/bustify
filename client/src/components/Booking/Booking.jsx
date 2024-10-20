d4e3512bc22478bd4d330e53ef67452459da9292
Yash Singhal <singhaly914@gmail.com>
2024-10-20 13:10:49 +0530

    update

--- client/src/components/Booking/Booking.jsx
+++ client/src/components/Booking/Booking.jsx
@@ -37,7 +37,7 @@ const Booking = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ amount: center.amount }),
+        body: JSON.stringify({ amount: center.amount*100 }),
       });
 
       const order = await response.json();
